16995217dfcc7c967d129feeb527f9460de54550
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2019-08-21 12:43:54 +0300

    Create 346_derinine arama.py

---
+++ Blm06-graf-algoritmaları/346_derinine arama.py
@@ -0,0 +1,34 @@
+
+
+
+graph1 = {
+    'A' : ['B','S'],
+    'B' : ['A'],
+    'C' : ['D','E','F','S'],
+    'D' : ['C'],
+    'E' : ['C','H'],
+    'F' : ['C','G'],
+    'G' : ['F','S'],
+    'H' : ['E','G'],
+    'S' : ['A','C','G']
+}
+ziydug = []
+graph2={
+    '1':['2','3','4'],
+    '2':['1','3','5'],
+    '3':['1','2','6'],
+    '4':['1'],
+    '5':['2'],
+    '6':['3']
+
+
+}
+
+def dfs(graph,node):
+    if node not in ziydug:
+        ziydug.append(node)
+        for n in graph[node]:
+            dfs(graph,n)
+
+dfs(graph2,'5')
+print(ziydug)
