221210c5abfc5682b9c2351d7aee1953454d2eef
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2019-08-28 16:04:38 +0300

    Update and rename Blm02-Temeller (Temel Algoritmalar)/blumblumshub.py to Blm02-Temeller (Temel Algoritmalar)/rundom_numbers/blumblumshub.py

--- Blm02-Temeller (Temel Algoritmalar)/rundom_numbers/blumblumshub.py
+++ Blm02-Temeller (Temel Algoritmalar)/rundom_numbers/blumblumshub.py
@@ -1,21 +1,22 @@
-import random
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 fig_size = [19.2,10.8]
 plt.figure(figsize=fig_size)
 arr = []
+K = 1000000000000000
 g = 6
 def blum(p,q,t):
     if(p == q or t < 1):
         print("error p = q")
         return 0
     m = p*q
-    s = int(random.random()*1000000000000000) % m
+    s = int(time.time()*K) % m
     z = 1
     while(z<=t):
         s = pow(s,2) % m
         arr.append(int(s % g))
-        print(int(s % g))
+        #print(int(s % g))
         z+=1
 
 blum(30000000091,40000000003,60000)
