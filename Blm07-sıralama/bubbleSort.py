d6d5e7b23f0e7a81180a84df9c483e864d44d5a4
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2019-09-04 11:45:19 +0300

    Update bubbleSort.py

--- Blm07-sıralama/bubbleSort.py
+++ Blm07-sıralama/bubbleSort.py
@@ -1,1 +1,17 @@
+# Bu kod KTÜ Bilgisayar Mühendisliği Bölümü'nde staj yapan öğrenciler tarafından hazırlanmıştır.
+# Yazar[lar]: Gizem Bostan, Seren Öztürk
+# Youtube :
 
+#bubble sort, gizem 
+def bubble_Sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(n-i-1):
+            if array[j] > array[j+1] :
+                array[j], array[j+1] = array[j+1], array[j]
+
+
+array= [64, 34, 25, 12, 22, 11, 90]
+bubble_Sort(array)
+print("Dizinin eleman sayisi:", len(array))
+print("Sorted array is:",array)
