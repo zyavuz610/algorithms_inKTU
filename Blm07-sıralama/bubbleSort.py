ee8308b8aaa62506467dc3542005ab32d488c6a7
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2019-09-05 06:18:36 +0300

    Update bubbleSort.py

--- Blm07-sıralama/bubbleSort.py
+++ Blm07-sıralama/bubbleSort.py
@@ -1,8 +1,7 @@
 # Bu kod KTÜ Bilgisayar Mühendisliği Bölümü'nde staj yapan öğrenciler tarafından hazırlanmıştır.
-# Yazar[lar]: Gizem Bostan, Seren Öztürk
-# Youtube :
+# Yazar[lar]: Gizem Bostan, Seren Öztürk, Emre Kardal, Ömer Melek, Ömer Çifte
 
-#bubble sort, gizem 
+#bubble sort 
 def bubble_Sort(array):
     n = len(array)
     for i in range(n):
@@ -10,8 +9,33 @@ def bubble_Sort(array):
             if array[j] > array[j+1] :
                 array[j], array[j+1] = array[j+1], array[j]
 
+def bidirectional_bubble_sort(array):
+    n = len(array)//2
+    while(n > 0):
+        m = n
+        while(m > 0):
+            i = 0
+            while(i < len(array) - m ):
+                if(array[i] > array[i + m]):
+                    array[i + m] -= array[i]
+                    array[i] += array[i + m]
+                    array[i + m] = array[i] - array[i + m]
+                i+=1
+            m = int(m/2)
+        n = int(n / 2)
+    flag = 1
+    while(flag == 1):
+        flag = 0
+        j = 0
+        while (j < len(array) - 1):
+            if (array[j] > array[j + 1]):
+                array[j + 1] -= array[j]
+                array[j] += array[j + 1]
+                array[j + 1] = array[j] - array[j + 1]
+                flag = 1
+            j+=1
 
 array= [64, 34, 25, 12, 22, 11, 90]
-bubble_Sort(array)
-print("Dizinin eleman sayisi:", len(array))
-print("Sorted array is:",array)
+# bubble_Sort(array)
+bidirectional_bubble_sort(array)
+print("Sıralı Dizi:",array)
