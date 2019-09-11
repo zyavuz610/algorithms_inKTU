499c3766483d2193a38d26c1b923b53bd4a8a811
Emre <36187735+EmreKARDAL@users.noreply.github.com>
2019-09-11 13:56:10 +0300

    Update rastgelesayıüretimi(zaralgoritması).py

--- Blm02-Temeller (Temel Algoritmalar)/rastgelesayıüretimi(zaralgoritması).py
+++ Blm02-Temeller (Temel Algoritmalar)/rastgelesayıüretimi(zaralgoritması).py
@@ -7,25 +7,22 @@ plt.figure(figsize=fig_size)
 arr = []
 a = time.time()
 z = 60000
-k = 10000
+k = 6
 b = 0
 arr = []
 if(z > 0):
     g = 0
     while(g < z):
-        a = (a * time.time()) + time.time()
-        a = a % 65536 + 1
-        c = int(((((a / 32768) + 1) / 4) * k) + 1)
+        a = (a*25173 + 13849) % 32768
+        c = int(((a / 32768)* k) + 1)
         b += c
         g += 1
         arr.append(c)
-        print(c)
+        #print(c)
     print("toplam puan")
     print(b)
 elif(z <= 0):
     print("zar atma sayısı 1 ve ya daha büyük olmalıdır")
-else:
-    print("Zarın yerde durabilmesi için minimum 3 köşesi olmalıdır")
 z = np.zeros(k+1)
 for i in arr:
     z[i] = z[i] + 1
