b5264165d16860fadd0c2e7a51e0d5b631bcbfa5
Emre <36187735+EmreKARDAL@users.noreply.github.com>
2019-09-11 12:28:08 +0300

    Update Non_recurcive_quicksort.py

--- Blm07-sıralama/Non_recurcive_quicksort.py
+++ Blm07-sıralama/Non_recurcive_quicksort.py
@@ -1,33 +1,45 @@
 import sys
-def hs(b,alt,üst):
-    alts = alt
-    üsts = üst
-    pivot = b[int((alt+üst)/2)]
-    kontrol3 = 1
-    while kontrol3 == 1 or alts <= üsts:
-        kontrol3 = 0
-        while b[üsts] > pivot:
-            üsts -= 1
-        while b[alts] < pivot:
-            alts += 1
-        if(alts <= üsts):
-            if(alts != üsts):
-                b[üsts],b[alts] = b[alts],b[üsts]
-            üsts-=1
-            alts+=1
-    if(alt<üsts):
-        hs(b,alt,üsts)
-    if(üst>alts):
-        hs(b,alts,üst)
-
 n = int(input())
 if (n<0):
     print("Error array size must positive number")
     sys.exit(1)
 b = []
+print("Enter numbers")
 for _ in range(n):
     x = float(input())
     b.append(x)
 print(b)
-hs(b,0,len(b)-1)
+altdizi = 0
+a = [[],[]]
+a[altdizi].append(0)
+a[altdizi].append(len(b) - 1)
+kontrol = 1
+while kontrol == 1 or altdizi >= 0:
+    kontrol = 0
+    alt = a[altdizi][0]
+    üst = a[altdizi][1]
+    altdizi -= 1
+    kontrol2 = 1
+    while kontrol2 == 1 or alt < üst:
+        kontrol2 = 0
+        alts = alt
+        üsts = üst
+        pivot = b[int((alt + üst)/2)]
+        kontrol3 = 1
+        while kontrol3 == 1 or alts <= üsts:
+            kontrol3 = 0
+            while b[alts] < pivot:
+                alts += 1
+            while b[üsts] > pivot:
+                üsts -= 1
+            if(alts <= üsts):
+                if(alts != üsts):
+                   b[üsts],b[alts] = b[alts],b[üsts]
+                alts += 1
+                üsts -= 1
+        if(alts <üst):
+            altdizi += 1
+            a[altdizi][0] = alts
+            a[altdizi][1] = üst
+        üst = üsts
 print(b)
