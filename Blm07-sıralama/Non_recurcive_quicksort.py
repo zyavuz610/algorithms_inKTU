d47da2c12402039bdb88b8d737d11a23859794ac
Emre <36187735+EmreKARDAL@users.noreply.github.com>
2019-09-11 12:38:17 +0300

    Update Non_recurcive_quicksort.py

--- Blm07-sıralama/Non_recurcive_quicksort.py
+++ Blm07-sıralama/Non_recurcive_quicksort.py
@@ -1,3 +1,6 @@
+# Bu kod KTÜ Bilgisayar Mühendisliği Bölümü'nde staj yapan öğrenciler tarafından hazırlanmıştır.
+# Yazar[lar]: Gizem Bostan, Seren Öztürk, Emre Kardal, Ömer Melek, Ömer Çifte
+
 import sys
 print("Enter the length of the array")
 n = int(input())
