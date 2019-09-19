a38bfaea13aced435d06e769579a4175613981db
Emre <36187735+EmreKARDAL@users.noreply.github.com>
2019-09-19 13:31:05 +0300

    Update birlestirmelisıralama.py

--- Blm07-sıralama/birlestirmelisıralama.py
+++ Blm07-sıralama/birlestirmelisıralama.py
@@ -1,31 +1,34 @@
 import sys
-def merge(alt,üst,b):
-    if(alt>=üst):
-        return 0
-    alts = alt
-    üsts = üst
-    pivot = int((alts + üsts) / 2)
-    merge(alts,pivot,b)
-    merge(pivot + 1,üsts,b)
-    sonalts = pivot
-    ilküsts = pivot + 1
-    while alts <= sonalts and ilküsts <= üsts:
-        if(b[alts]< b[ilküsts]):
-            alts += 1
-        else:
-            t = b[ilküsts]
-            k = ilküsts - 1
-            while k >= alts:
-                b[k + 1] = b[k]
-                k -= 1
-            b[alts] = t
-            alts += 1
-            sonalts += 1
-            ilküsts += 1
+class Merge:
+    def merge(self,alt, üst, b):
+        if (alt >= üst):
+            return 0
+        alts = alt
+        üsts = üst
+        pivot = int((alts + üsts) / 2)
+        self.merge(alts, pivot, b)
+        self.merge(pivot + 1, üsts, b)
+        sonalts = pivot
+        ilküsts = pivot + 1
+        while alts <= sonalts and ilküsts <= üsts:
+            if (b[alts] < b[ilküsts]):
+                alts += 1
+            else:
+                t = b[ilküsts]
+                k = ilküsts - 1
+                while k >= alts:
+                    b[k + 1] = b[k]
+                    k -= 1
+                b[alts] = t
+                alts += 1
+                sonalts += 1
+                ilküsts += 1
+        return b
+
 
 print("Enter the length of the array")
 n = int(input())
-if (n<0):
+if (n < 0):
     print("Error array size must positive number")
     sys.exit(1)
 b = []
@@ -34,5 +37,5 @@ for _ in range(n):
     x = float(input())
     b.append(x)
 print(b)
-merge(0,len(b) - 1,b)
-print(b)
+z = Merge()
+print(z.merge(0, len(b) - 1, b))
